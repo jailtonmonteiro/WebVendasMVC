b871abea31e9b2273b93c09ebb271d1eebb6187c
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-08-03 17:05:39 -0300

    Construção da página de Detalhes do vendedor

--- WebVendasMvc/Service/SellerService.cs
+++ WebVendasMvc/Service/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using WebVendasMvc.Data;
 using WebVendasMvc.Models;
 
@@ -27,7 +28,7 @@ namespace WebVendasMvc.Service
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
