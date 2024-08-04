ac6a3c20b1a2eb3b2e3ad6844272c3af3d7c5432
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-08-04 16:32:26 -0300

    Inserindo validações de campos

--- WebVendasMvc/Models/Seller.cs
+++ WebVendasMvc/Models/Seller.cs
@@ -9,22 +9,29 @@ namespace WebVendasMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Campo Obrigatório.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do nome não atende aos requisitos: minimo {2} e máximo {1}")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Insira um email válido.")]
         public string Email { get; set; }
 
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Campo Obrigatório.")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "Campo Obrigatório.")]
         public double BaseSalary { get; set; }
 
         [Display(Name = "Departamento")]
+        [Required(ErrorMessage = "Campo Obrigatório.")]
         public Department Department { get; set; }
         public int DepartmentID { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
